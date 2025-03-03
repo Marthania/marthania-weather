cf4c898a3a0242397c334e9a8173e2040b8c559e
Marthania <chibowamartha@gmail.com>
2025-03-03 17:40:57 +0200

    updated weather data

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ function updateWeatherData(response){
     let temperatureElement=document.querySelector("#temperature");
     let temperature=response.data.temperature.current;
     let cityElement=document.querySelector("#city");
-    
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind");
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time*1000);
+    console.log(response.data);
+
+
+    timeElement.innerHTML=formatDate(date);
+    humidityElement.innerHTML=response.data.temperature.humidity;
+    windElement.innerHTML=response.data.wind.speed;
+    descriptionElement.innerHTML=response.data.condition.description;
     cityElement.innerHTML=response.data.city;
     temperatureElement.innerHTML=Math.round(temperature);
+}
+//format date
+function formatDate(date){
+    let hours=date.getHours();
+    if(hours<10){
+        hours=`0${hours}`;
+    }
+    let minutes=date.getMinutes();
+    if(minutes<10){
+        minutes=`0${minutes}`;
+    }
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day=days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
 }
 //api key
 function searchCity(city){
