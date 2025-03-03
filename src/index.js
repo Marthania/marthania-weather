16a753de12b66bd77f1144b8ef33abc166da15c2
Marthania <chibowamartha@gmail.com>
2025-03-03 16:39:56 +0200

    added js to the search engine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-
+//weather data
+function updateWeatherData(response){
+    console.log(response.data.temperature.current);
+    let temperatureElement=document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    let cityElement=document.querySelector("#city");
+    
+    cityElement.innerHTML=response.data.city;
+    temperatureElement.innerHTML=Math.round(temperature);
+}
+//api key
+function searchCity(city){
+    let apiKey  = "88bf2d049e3o03c506t04489abf0bb1a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeatherData); 
+    }
 //searchForm
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("Bulawayo")
