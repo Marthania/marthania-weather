32101d2a69c3fcf546a715e85cf676d7873e4136
Marthania <chibowamartha@gmail.com>
2025-03-03 18:04:58 +0200

    updated time and weather details

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ function updateWeatherData(response){
     let windElement=document.querySelector("#wind");
     let timeElement=document.querySelector("#time");
     let date=new Date(response.data.time*1000);
+    let iconElement=document.querySelector("#icon");
     console.log(response.data);
 
 
+    iconElement.innerHTML=`<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     timeElement.innerHTML=formatDate(date);
     humidityElement.innerHTML=response.data.temperature.humidity;
     windElement.innerHTML=response.data.wind.speed;
